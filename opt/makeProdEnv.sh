bfeb5ca9d329232d737344fc4ad5bece8107d872
Martin Neundorfer <martin.neundorfer@zinal-consulting.de>
2023-12-11 10:05:33 +0100

    fix: adjust script for new project architecture

--- opt/makeProdEnv.sh
+++ opt/makeProdEnv.sh
@@ -6,41 +6,8 @@ if [ -z "$1" ];  then
 fi
 
 projectEnv=$1
-
-# Remove current env file
-rm -f .env
-
 emptyLineReg="^\s+$"
 emptyValueReg=".*?=($|\s*$)"
-
-# Read the .env.template file
-out=""
-if [ -e .env.template ]; then
-  for LINE in `cat .env.template`; do
-    if [[ $LINE =~ $emptyLineReg ]]; then continue; fi
-    if [[ $LINE =~ $emptyValueReg ]]; then continue; fi
-
-    out+="$LINE
-"
-  done
-  echo "  [+] .env.template contents added"
-else
-  echo "  [?] No .env.template found, skip..."
-fi
-
-# Read the .env.app.template file
-if [ -e .env.app.template ]; then
-  for LINE in `cat .env.app.template`; do
-    if [[ $LINE =~ $emptyLineReg ]]; then continue; fi
-    if [[ $LINE =~ $emptyValueReg ]]; then continue; fi
-
-    out+="$LINE
-"
-  done
-  echo "  [+] .env.app.template contents added"
-else
-  echo "  [?] No .env.app.template found, skip..."
-fi
 
 # Read .env.prod file
 if [ -e ".env.$projectEnv" ]; then
