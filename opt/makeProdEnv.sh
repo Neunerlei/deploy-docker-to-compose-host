13a5b28a14c102a2cd7141b206c27da4deb1b27c
Martin Neundorfer <martin.neundorfer@zinal-consulting.de>
2023-12-11 10:10:49 +0100

    fix: read .env file again

--- opt/makeProdEnv.sh
+++ opt/makeProdEnv.sh
@@ -9,6 +9,21 @@ projectEnv=$1
 emptyLineReg="^\s+$"
 emptyValueReg=".*?=($|\s*$)"
 
+# Read the .env file
+out=""
+if [ -e .env ]; then
+  for LINE in `cat .env`; do
+    if [[ $LINE =~ $emptyLineReg ]]; then continue; fi
+    if [[ $LINE =~ $emptyValueReg ]]; then continue; fi
+
+    out+="$LINE
+"
+  done
+  echo "  [+] .env contents added"
+else
+  echo "  [?] No .env found, skip..."
+fi
+
 # Read .env.prod file
 if [ -e ".env.$projectEnv" ]; then
 	for LINE in `cat .env.$projectEnv`; do
